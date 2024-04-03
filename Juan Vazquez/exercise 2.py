a2f1ae0b8541f78ef665547dc320a327633ceb34
Juan Vazquez <ac_39vaz@hotmail.com>
2024-04-02 21:29:48 -0600

    Corregido

--- Juan Vazquez/exercise 2.py
+++ Juan Vazquez/exercise 2.py
@@ -6,9 +6,7 @@ Quantity_of_A = int(input('Pyramid height:  '))
 
 #range top-down
 for i in range(0, Quantity_of_A):
-    i = i
     print('A'* i)
 #range bottom up inverted    
 for i in range(Quantity_of_A, -1, -1):
-    i = i
     print('A'* i)
